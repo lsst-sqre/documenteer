2c179160715e6384d41ddf119e6df965ed53eefe
Jonathan Sick <jsick@lsst.org>
2016-05-23 10:09:27 -0700

    Add documenteer.sphinxext subpackage
    This sub-package will contain various Sphinx extensions (roles,
    directives) for LSST DM documentation projects.

---
+++ documenteer/sphinxext/__init__.py
@@ -0,0 +1,11 @@
+"""Sphinx/docutils extensions for LSST DM documentation."""
+"""Sphinx/docutils extensions for LSST DM documentation.
+
+Enable these extension by adding `documenteer.sphinxext` to your
+extensions list in :file:`conf.py`::
+
+    extensions = [
+       # ...
+       'documenteer.sphinxext'
+    ]
+"""
