bea74eff26ca329a1d4cb699308aba3d5c625107
Jonathan Sick <jsick@lsst.org>
2016-05-27 14:17:40 -0700

    Bump to version 0.1.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ author = 'Jonathan Sick'
 author_email = 'jsick@lsst.org'
 license = 'MIT'
 url = 'https://github.com/lsst-sqre/documenteer'
-version = '0.1.3'
+version = '0.1.4'
 
 
 def read(filename):
