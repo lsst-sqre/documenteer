f5d572c2aaa6db7273cdb75c2324077038c50bac
Jonathan Sick <jsick@lsst.org>
2017-07-20 09:31:46 -0400

    Pin docutils==0.13.1
    This is necessary because the Sphinx>=1.5.0,<1.6.0 constraint
    effectively requires docutils 0.13.1, but this isn't rigorously enforced
    by Sphinx's own requirements. This pin will need to change when we
    upgrade to the Sphinx 1.6.0 series.

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ long_description = read('README.rst')
 # Core dependencies
 install_requires = [
     'future',
+    'docutils==0.13.1',  # currently pinned in conjunction with Sphinx
     'Sphinx>=1.5.0,<1.6.0',
     'PyYAML',
     'sphinx-prompt',
