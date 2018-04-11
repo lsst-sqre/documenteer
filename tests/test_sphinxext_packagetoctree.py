1c0f71d41651ac573edfa305c62a5422e92a7d4e
Jonathan Sick <jsick@lsst.org>
2018-04-11 12:15:34 -0700

    Add module-toctree and package-toctree directives
    The module-toctree directive creates a toctree for modules
    specifically in the pipelines_lsst_io project.
    
    The module-toctree directive creates a toctree for packages
    specifically in the pipelines_lsst_io project.

---
+++ tests/test_sphinxext_packagetoctree.py
@@ -0,0 +1,22 @@
+"""Tests for documenteer.sphinext.packagetoctree (package-toctree and
+module-toctree directives).
+"""
+
+from documenteer.sphinxext.packagetoctree import _filter_index_pages
+
+
+def test_filter_index_pages():
+    """Test _filter_index_pages.
+    """
+    docnames = [
+        'index',
+        'basedir/A/index',
+        'basedir/B/index',
+        'basedir/B/subdir/index'
+        'otherdir/C/index'
+    ]
+    expected = [
+        'basedir/A/index',
+        'basedir/B/index',
+    ]
+    assert set(expected) == set(list(_filter_index_pages(docnames, 'basedir')))
