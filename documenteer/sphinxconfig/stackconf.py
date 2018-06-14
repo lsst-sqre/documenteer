0d944bc7f43aa699a736ea353d10746a0dec038d
Jonathan Sick <jsick@lsst.org>
2018-06-14 13:21:26 -0500

    Refactor stackconfig by topic
    Refactoring build_package_configs should make it easier to reuse
    configurations for the primary Sphinx build in pipelines_lsst_io.

--- documenteer/sphinxconfig/stackconf.py
+++ documenteer/sphinxconfig/stackconf.py
@@ -1,10 +1,15 @@
-# Some configurations based on astropy-helpers
-# https://github.com/astropy/astropy-helpers/blob/master/astropy_helpers/sphinx/conf.py
-# see licenses/astropy-helpers.txt
-"""Sphinx configuration defaults for LSST Stack packages."""
+"""Sphinx configuration defaults for LSST Stack packages.
+
+Notes
+-----
+Some configurations based on astropy-helpers
+https://github.com/astropy/astropy-helpers/blob/master/astropy_helpers/sphinx/conf.py
+see licenses/astropy-helpers.txt
+"""
+
+__all__ = ('build_package_configs',)
 
 import datetime
-import sys
 import warnings
 
 import lsst_sphinx_bootstrap_theme
@@ -12,74 +17,9 @@ import lsst_sphinx_bootstrap_theme
 from .utils import read_git_commit_timestamp
 
 
-def build_package_configs(project_name,
-                          version='unknown',
-                          copyright=None,
-                          doxygen_xml_dirname=None):
-    """Builds a `dict` of Sphinx configurations useful for the ``doc/conf.py``
-    files of individual LSST Stack packages.
-
-    The ``doc/conf.py`` of packages can ingest these configurations via
-
-    .. code:: python
-
-       from documenteer.sphinxconfing.stackconf import build_package_configs
-
-       _g = globals()
-       _g.update(build_package_configs(
-           project_name='afw',
-           version=lsst.afw.version.__version__))
-
-    You can subsequently customize the Sphinx configuration by directly
-    assigning global variables, as usual in a Sphinx ``config.py``, e.g.:
-
-    .. code:: python
-
-       copyright = '2016 Association of Universities for '
-                   'Research in Astronomy, Inc.'
-
-    Parameters
-    ----------
-    project_name : str
-        Name of the package.
-    copyright : str, optional
-        Copyright statement. Do not include the 'Copyright (c)' string; it'll
-        be added automatically.
-    version : str
-        Version string. Use the ``__version__`` member in a package's
-        ``version`` module.
-    doxygen_xml_dirname : str
-        Path to doxygen-generated XML, allowing C++ APIs to be documented
-        through breathe. If not set, the breathe sphinx extension will not be
-        enabled.
-
-    Returns
-    -------
-    c : dict
-        Dictionary of configurations that should be added to the ``conf.py``
-        global namespace via::
-
-            _g = global()
-            _g.update(c)
+def _insert_extensions(c):
+    """Insert the ``extensions`` variable into the configuration state.
     """
-    try:
-        date = read_git_commit_timestamp()
-    except Exception:
-        date = datetime.datetime.now()
-
-    c = {}
-    c['project'] = project_name
-    if copyright is not None:
-        c['copyright'] = copyright
-    else:
-        c['copyright'] = 'Copyright {:s} LSST contributors.'.format(
-            date.strftime('%Y-%m-%d'))
-    c['version'] = version
-    c['release'] = version
-
-    c['today'] = date.strftime('%Y-%m-%d')
-
-    # Sphinx extension modules
     c['extensions'] = [
         'sphinx.ext.autodoc',
         'sphinx.ext.doctest',
@@ -98,27 +38,16 @@ def build_package_configs(project_name,
         'astropy_helpers.sphinx.ext.doctest',
         'astropy_helpers.sphinx.ext.changelog_links',
         'astropy_helpers.extern.automodapi.smart_resolver',
+        'breathe',
         'documenteer.sphinxext',
     ]
+    return c
 
-    # The suffix(es) of source filenames.
-    # You can specify multiple suffix as a list of string:
-    c['source_suffix'] = '.rst'
 
-    # The encoding of source files.
-    c['source_encoding'] = 'utf-8-sig'
-
-    # The master toctree document.
-    c['master_doc'] = 'index'
-
-    # List of patterns, relative to source directory, that match files and
-    # directories to ignore when looking for source files.
-    c['exclude_patterns'] = ['_build', 'README.rst']
-
-    # If true, `todo` and `todoList` produce output, else they produce nothing.
-    c['todo_include_todos'] = True
-
-    # Configuration for Intersphinx
+def _insert_intersphinx_mapping(c):
+    """Insert the ``intersphinx_mapping``, ``intersphinx_timeout`` and
+    ``intersphinx_cache_limit`` variableis into the configuration state.
+    """
     c['intersphinx_mapping'] = {
         'python': ('http://docs.python.org/3/', None),
         # FIXME add local object cache
@@ -132,87 +61,20 @@ def build_package_configs(project_name,
         'astropy': ('http://docs.astropy.org/en/stable/', None),
         'h5py': ('http://docs.h5py.org/en/latest/', None)
     }
+    c['intersphinx_timeout'] = 10.0  # seconds
+    c['intersphinx_cache_limit'] = 5  # days
+    return c
 
-    # Revert python intersphinx to Py 2 if that's the build environment
-    if sys.version_info[0] == 2:
-        c['intersphinx_mapping']['python'] = ('http://docs.python.org/2/',
-                                              None)
-        # FIXME add local object cache
-        # c['intersphinx_mapping']['pythonloc'] = (
-        #     'http://docs.python.org/',
-        #     os.path.abspath(os.path.join(os.path.dirname(__file__),
-        #                     'local/python2_local_links.inv')))
 
-    # The reST default role (used for this markup: `text`)
-    c['default_role'] = 'obj'
-
-    # This is added to the end of RST files - a good place to put substitutions
-    # to be used globally.
-    c['rst_epilog'] = """
-.. _Astropy: http://astropy.org
+def _insert_html_configs(c, *, project_name, short_project_name):
+    """Insert HTML theme configurations.
     """
-
-    # A list of warning types to suppress arbitrary warning messages. We mean
-    # to override directives in
-    # astropy_helpers.sphinx.ext.autodoc_enhancements, thus need to ignore
-    # those warning. This can be removed once the patch gets released in
-    # upstream Sphinx (https://github.com/sphinx-doc/sphinx/pull/1843).
-    # Suppress the warnings requires Sphinx v1.4.2
-    c['suppress_warnings'] = ['app.add_directive', ]
-
-    try:
-        import matplotlib.sphinxext.plot_directive
-        c['extensions'] += [matplotlib.sphinxext.plot_directive.__name__]
-    except (ImportError, AttributeError):
-        # AttributeError is checked here in case matplotlib is installed but
-        # Sphinx isn't.  Note that this module is imported by the config file
-        # generator, even if we're not building the docs.
-        warnings.warn(
-            "matplotlib's plot_directive could not be imported. " +
-            "Inline plots will not be included in the output")
-
-    # Don't show summaries of the members in each class along with the
-    # class' docstring
-    c['numpydoc_show_class_members'] = False
-
-    c['autosummary_generate'] = True
-
-    c['automodapi_toctreedirnm'] = 'py-api'
-
-    # Class documentation should contain *both* the class docstring and
-    # the __init__ docstring
-    c['autoclass_content'] = "both"
-
-    # Render inheritance diagrams in SVG
-    c['graphviz_output_format'] = "svg"
-
-    c['graphviz_dot_args'] = [
-        '-Nfontsize=10',
-        '-Nfontname=Helvetica Neue, Helvetica, Arial, sans-serif',
-        '-Efontsize=10',
-        '-Efontname=Helvetica Neue, Helvetica, Arial, sans-serif',
-        '-Gfontsize=10',
-        '-Gfontname=Helvetica Neue, Helvetica, Arial, sans-serif'
-    ]
-
-    if doxygen_xml_dirname is not None:
-        c['extensions'].append('breathe')
-        c['breathe_projects'] = {project_name: doxygen_xml_dirname}
-        c['breathe_default_project'] = project_name
-
-    # -- Options for HTML output ----------------------------------------------
-
+    # Use the lsst-sphinx-bootstrap-theme
     c['templates_path'] = [
         '_templates',
         lsst_sphinx_bootstrap_theme.get_html_templates_path()]
-
     c['html_theme'] = 'lsst_sphinx_bootstrap_theme'
     c['html_theme_path'] = [lsst_sphinx_bootstrap_theme.get_html_theme_path()]
-
-    c['numfig'] = True
-    c['numfig_format'] = {'figure': 'Figure %s',
-                          'table': 'Table %s',
-                          'code-block': 'Listing %s'}
 
     # Theme options are theme-specific and customize the look and feel of a
     # theme further.  For a list of options available for each theme, see the
@@ -221,11 +83,11 @@ def build_package_configs(project_name,
 
     # The name for this set of Sphinx documents.  If None, it defaults to
     # "<project> v<release> documentation".
-    c['html_title'] = c['project']
+    c['html_title'] = project_name
 
     # A shorter title for the navigation bar.  Default is the same as
     # html_title.
-    c['html_short_title'] = c['project']
+    c['html_short_title'] = short_project_name
 
     # The name of an image file (relative to this directory) to place at the
     # top of the sidebar.
@@ -294,5 +156,223 @@ def build_package_configs(project_name,
     # that implements a search results scorer. If empty, the default will be
     # used.
     # html_search_scorer = 'scorer.js'
+
+    return c
+
+
+def _insert_common_sphinx_configs(c, *, project_name):
+    """Add common core Sphinx configurations to the state.
+    """
+    c['project'] = project_name
+
+    # The suffix(es) of source filenames.
+    # You can specify multiple suffix as a list of string:
+    c['source_suffix'] = '.rst'
+
+    # The encoding of source files.
+    c['source_encoding'] = 'utf-8-sig'
+
+    # The master toctree document.
+    c['master_doc'] = 'index'
+
+    # Configure figure numbering
+    c['numfig'] = True
+    c['numfig_format'] = {'figure': 'Figure %s',
+                          'table': 'Table %s',
+                          'code-block': 'Listing %s'}
+
+    # The reST default role (used for this markup: `text`)
+    c['default_role'] = 'obj'
+
+    # This is added to the end of RST files - a good place to put substitutions
+    # to be used globally.
+    c['rst_epilog'] = """
+.. _Astropy: http://astropy.org
+    """
+
+    # A list of warning types to suppress arbitrary warning messages. We mean
+    # to override directives in
+    # astropy_helpers.sphinx.ext.autodoc_enhancements, thus need to ignore
+    # those warning. This can be removed once the patch gets released in
+    # upstream Sphinx (https://github.com/sphinx-doc/sphinx/pull/1843).
+    # Suppress the warnings requires Sphinx v1.4.2
+    c['suppress_warnings'] = ['app.add_directive', ]
+
+    return c
+
+
+def _insert_breathe_configs(c, *, project_name, doxygen_xml_dirname):
+    """Add breathe extension configurations to the state.
+    """
+    if doxygen_xml_dirname is not None:
+        c['breathe_projects'] = {project_name: doxygen_xml_dirname}
+        c['breathe_default_project'] = project_name
+    return c
+
+
+def _insert_automodapi_configs(c):
+    """Add configurations related to automodapi and numpydoc to the state.
+    """
+    # Don't show summaries of the members in each class along with the
+    # class' docstring
+    c['numpydoc_show_class_members'] = False
+
+    c['autosummary_generate'] = True
+
+    c['automodapi_toctreedirnm'] = 'py-api'
+
+    # Class documentation should contain *both* the class docstring and
+    # the __init__ docstring
+    c['autoclass_content'] = "both"
+
+    return c
+
+
+def _insert_matplotlib_configs(c):
+    """Add configurations related to matplotlib's plot directive to the state.
+    """
+    if 'extensions' not in c:
+        c['extensions'] = []
+
+    try:
+        import matplotlib.sphinxext.plot_directive
+        c['extensions'] += [matplotlib.sphinxext.plot_directive.__name__]
+    except (ImportError, AttributeError):
+        # AttributeError is checked here in case matplotlib is installed but
+        # Sphinx isn't.  Note that this module is imported by the config file
+        # generator, even if we're not building the docs.
+        warnings.warn(
+            "matplotlib's plot_directive could not be imported. "
+            "Inline plots will not be included in the output.")
+
+    return c
+
+
+def _insert_graphviz_configs(c):
+    """Insert configurations for graphviz to the state.
+
+    Graphviz is primarily used by automodapi to create dependency
+    diagrams for Python.
+    """
+    # Render inheritance diagrams in SVG
+    c['graphviz_output_format'] = "svg"
+
+    c['graphviz_dot_args'] = [
+        '-Nfontsize=10',
+        '-Nfontname=Helvetica Neue, Helvetica, Arial, sans-serif',
+        '-Efontsize=10',
+        '-Efontname=Helvetica Neue, Helvetica, Arial, sans-serif',
+        '-Gfontsize=10',
+        '-Gfontname=Helvetica Neue, Helvetica, Arial, sans-serif'
+    ]
+
+    return c
+
+
+def build_package_configs(project_name,
+                          version='unknown',
+                          copyright=None,
+                          doxygen_xml_dirname=None):
+    """Builds a `dict` of Sphinx configurations useful for the ``doc/conf.py``
+    files of individual LSST Stack packages.
+
+    The ``doc/conf.py`` of packages can ingest these configurations via
+
+    .. code:: python
+
+       from documenteer.sphinxconfing.stackconf import build_package_configs
+
+       _g = globals()
+       _g.update(build_package_configs(
+           project_name='afw',
+           version=lsst.afw.version.__version__))
+
+    You can subsequently customize the Sphinx configuration by directly
+    assigning global variables, as usual in a Sphinx ``config.py``, e.g.:
+
+    .. code:: python
+
+       copyright = '2016 Association of Universities for '
+                   'Research in Astronomy, Inc.'
+
+    Parameters
+    ----------
+    project_name : str
+        Name of the package.
+    copyright : str, optional
+        Copyright statement. Do not include the 'Copyright (c)' string; it'll
+        be added automatically.
+    version : str
+        Version string. Use the ``__version__`` member in a package's
+        ``version`` module.
+    doxygen_xml_dirname : str
+        Path to doxygen-generated XML, allowing C++ APIs to be documented
+        through breathe. If not set, the breathe sphinx extension will not be
+        enabled.
+
+    Returns
+    -------
+    c : dict
+        Dictionary of configurations that should be added to the ``conf.py``
+        global namespace via::
+
+            _g = global()
+            _g.update(c)
+    """
+    c = {}
+
+    c = _insert_common_sphinx_configs(
+        c,
+        project_name=project_name)
+
+    # HTML theme
+    c = _insert_html_configs(
+        c,
+        project_name=project_name,
+        short_project_name=project_name)
+
+    # Sphinx extension modules
+    c = _insert_extensions(c)
+
+    # Intersphinx configuration
+    c = _insert_intersphinx_mapping(c)
+
+    # Breathe extension configuration
+    c = _insert_breathe_configs(
+        c,
+        project_name=project_name,
+        doxygen_xml_dirname=doxygen_xml_dirname)
+
+    # Automodapi and numpydoc configurations
+    c = _insert_automodapi_configs(c)
+
+    # Matplotlib configurations
+    c = _insert_matplotlib_configs(c)
+
+    # Graphviz configurations
+    c = _insert_graphviz_configs(c)
+
+    try:
+        date = read_git_commit_timestamp()
+    except Exception:
+        date = datetime.datetime.now()
+
+    if copyright is not None:
+        c['copyright'] = copyright
+    else:
+        c['copyright'] = 'Copyright {:s} LSST contributors.'.format(
+            date.strftime('%Y-%m-%d'))
+    c['version'] = version
+    c['release'] = version
+
+    c['today'] = date.strftime('%Y-%m-%d')
+
+    # List of patterns, relative to source directory, that match files and
+    # directories to ignore when looking for source files.
+    c['exclude_patterns'] = ['_build', 'README.rst']
+
+    # Show rendered todo directives in package docs since they're developer
+    # facing.
+    c['todo_include_todos'] = True
 
     return c
