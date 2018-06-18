45dafb73b47f6ab2aab7faeed0e31b5bb3f96055
Jonathan Sick <jsick@lsst.org>
2018-06-18 13:16:16 -0500

    Add conf.py discovery to stack-docs
    discover_conf_py_directory works like discover_package_doc_dir, but
    specifically for stack-docs CLI where there are no doc/ directories to
    discover.

--- tests/test_stackdocs_rootdiscovery.py
+++ tests/test_stackdocs_rootdiscovery.py
@@ -8,7 +8,8 @@ import tempfile
 import pytest
 
 from documenteer.stackdocs.rootdiscovery import (
-    _has_conf_py, _search_parents, discover_package_doc_dir)
+    _has_conf_py, _search_parents, discover_package_doc_dir,
+    discover_conf_py_directory)
 
 
 def test_has_conf_py():
@@ -81,6 +82,37 @@ def test_discover_package_doc_dir_toplevel_doc():
         _install_conf_py(root_dir / 'doc')
         expected = pathlib.Path(tempdir).resolve() / 'doc'
         assert discover_package_doc_dir(tempdir) == str(expected)
+
+
+def test_discover_conf_py_directory():
+    """Test discover_conf_py_directory when a conf.py exists in the initial
+    directory.
+    """
+    with tempfile.TemporaryDirectory() as tempdir:
+        root_dir = pathlib.Path(tempdir)
+        _install_conf_py(root_dir)
+        expected = pathlib.Path(tempdir).resolve()
+        assert discover_conf_py_directory(tempdir) == str(expected)
+
+
+def test_discover_conf_py_directory_not_found():
+    """Test discover_conf_py_directory when a conf.py does not exist.
+    """
+    with tempfile.TemporaryDirectory() as tempdir:
+        with pytest.raises(FileNotFoundError):
+            discover_conf_py_directory(tempdir)
+
+
+def test_discover_conf_py_directory_search_parents():
+    """Test discover_conf_py_directory when conf.py is in a parent directory.
+    """
+    with tempfile.TemporaryDirectory() as tempdir:
+        root_dir = pathlib.Path(tempdir)
+        _install_conf_py(root_dir)
+        os.makedirs(root_dir / 'a' / 'b')
+        expected = pathlib.Path(tempdir).resolve()
+        assert discover_conf_py_directory(root_dir / 'a' / 'b') \
+            == str(expected)
 
 
 def _install_conf_py(directory):
