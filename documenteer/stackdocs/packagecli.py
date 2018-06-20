de5546cfaa75625accb5171c3bdd10d782b757fb
Jonathan Sick <jsick@lsst.org>
2018-06-20 13:56:57 -0500

    Improve docs for stack-docs and package-docs
    This content is used both for command-line help and to generate
    documenteer.lsst.io-based help.

--- documenteer/stackdocs/packagecli.py
+++ documenteer/stackdocs/packagecli.py
@@ -26,19 +26,39 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
                     resolve_path=True),
     default='.',
     help="Root Sphinx doc/ directory. You don't need to set this argument "
-         "explicitly as long as the current working directory is either the "
-         "root of the package, the doc/ directory, or a subdirectory of doc/."
+         "explicitly as long as the current working directory is any of:\n\n"
+         "- the root of the package\n"
+         "- the doc/ directory\n"
+         "- a subdirectory of doc/\n"
 )
 @click.option(
     '-v', '--verbose',
     is_flag=True,
-    help='Enable verbose output (debug-level logging)'
+    help='Enable verbose output (debug-level logging).'
 )
 @click.version_option()
 @click.pass_context
 def main(ctx, root_dir, verbose):
     """package-docs is a CLI for building single-package previews of
     documentation in the LSST Stack.
+
+    Use package-docs during development to quickly preview your documentation
+    and docstrings.
+
+    .. warning::
+
+       Using package-docs to compile standalone documentation for a single
+       package will generate warnings related to missing references. This is
+       normal because the full documentation set is not built in the mode.
+       Before shipping revised documentation for a package, always make sure
+       cross-package references work by doing a full-site build either locally
+       with the stack-docs CLI or the site's Jenkins job.
+
+    The key commands provided by package-docs are:
+
+    - ``package-docs build``: compile the package's documentation.
+    - ``package-docs clean``: removes documentation build products from a
+      package.
     """
     root_dir = discover_package_doc_dir(root_dir)
 
@@ -76,6 +96,9 @@ def help(ctx, topic, **kw):
 @click.pass_context
 def build(ctx):
     """Build documentation as HTML.
+
+    The build HTML site is located in the ``doc/_build/html`` directory
+    of the package.
     """
     return_code = run_sphinx(ctx.obj['root_dir'])
     if return_code > 0:
@@ -86,6 +109,15 @@ def build(ctx):
 @click.pass_context
 def clean(ctx):
     """Clean Sphinx build products.
+
+    Use this command to clean out build products after a failed build, or
+    in preparation for running a build from a clean state.
+
+    This command removes the following directories from the package's doc/
+    directory:
+
+    - ``_build`` (the Sphinx build itself)
+    - ``py-api`` (pages created by automodapi for the Python API reference)
     """
     logger = logging.getLogger(__name__)
 
--- documenteer/stackdocs/stackcli.py
+++ documenteer/stackdocs/stackcli.py
@@ -25,19 +25,39 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
     default='.',
     help="Root Sphinx project directory. You don't need to set this argument "
          "explicitly as long as the current working directory is the "
-         "main documentation repo (pipelines_lsst_io for example) or a "
+         "main documentation repo (``pipelines_lsst_io`` for example) or a "
          "subdirectory of it."
 )
 @click.option(
     '-v', '--verbose',
     is_flag=True,
-    help='Enable verbose output (debug-level logging)'
+    help='Enable verbose output (debug-level logging).'
 )
 @click.version_option()
 @click.pass_context
 def main(ctx, root_project_dir, verbose):
     """stack-docs is a CLI for building LSST Stack documentation, such as
     pipelines.lsst.io.
+
+    This command should be run on the "main" documentation repository, namely
+    https://github.com/lsst/pipelines_lsst_io.
+
+    The stack-docs command replaces the usual Makefile and sphinx-build system
+    for Sphinx projects. This dedicated tool provide subcommands that are
+    engineered specifically for building the ``pipelines_lsst_io`` project.
+
+    The key commands provided by stack-docs are:
+
+    - ``stack-docs build``: compile the pipelines.lsst.io site from the
+      ``pipelines_lsst_io`` repository and linked packages.
+
+    - ``stack-docs clean``: removes build products. Use this command to
+      clear the build cache.
+
+    See also: package-docs, a tool for building previews of package
+    documentation.
+
+    For more information about stack-docs, see https://documenteer.lsst.io.
     """
     root_project_dir = discover_conf_py_directory(root_project_dir)
 
@@ -81,6 +101,26 @@ def help(ctx, topic, **kw):
 @click.pass_context
 def build(ctx, skip):
     """Build documentation as HTML.
+
+    This command performs these steps:
+
+    1. Removes any existing symlinks in the ``modules``, ``packages``, and
+       ``_static`` directories.
+
+    2. Finds packages set up by EUPS that have Sphinx-enabled doc/ directories
+       and links their module and package directories into the
+       ``pipelines_lsst_io`` repository. The ``doc/manifest.yaml`` file in
+       each package is what defines the package and module documentation
+       directories for each package.
+
+    3. Run a single, monolithic Sphinx build on the ``pipelines_lsst_io``
+       repository and linked packages.
+
+    By default, the build site is located in the ``_build/html`` directory
+    of the ``pipelines_lsst_io`` repository.
+
+    To peek inside the build process, see the ``documenteer.stackdocs.build``
+    APIs.
     """
     return_code = build_stack_docs(
         ctx.obj['root_project_dir'],
@@ -93,6 +133,17 @@ def build(ctx, skip):
 @click.pass_context
 def clean(ctx):
     """Clean Sphinx build products.
+
+    Use this command to clean out build products after a failed build, or
+    in preparation for running a build from a clean state.
+
+    This command removes the following directories from the
+    ``pipelines_lsst_io`` directory:
+
+    - ``_build`` (the Sphinx build itself)
+    - ``modules`` (symlinks to the module doc directories of Stack packages)
+    - ``packages`` (symlinks to the package doc directories of Stack packages)
+    - ``py-api`` (pages created by automodapi for the Python API reference)
     """
     logger = logging.getLogger(__name__)
 
