3cedca6f5256f5b5f866c75cf7e0a1f4d827c23d
Jonathan Sick <jsick@lsst.org>
2018-09-03 18:20:08 -0500

    Add format_field_nodes function
    This function creates docutils nodes for an lsst.pex.config.Field
    configuration field.

--- documenteer/sphinxext/lssttasks/formatters.py
+++ documenteer/sphinxext/lssttasks/formatters.py
@@ -2,9 +2,10 @@
 """
 
 __all__ = (
-    'get_field_formatter', 'format_configurablefield_nodes',
-    'create_field_type_item_node', 'create_default_target_item_node',
-    'create_description_node'
+    'get_field_formatter', 'format_field_nodes',
+    'format_configurablefield_nodes', 'create_dtype_item_node',
+    'create_field_type_item_node', 'create_default_item_node',
+    'create_default_target_item_node', 'create_description_node'
 )
 
 from docutils import nodes
@@ -48,6 +49,52 @@ def get_field_formatter(field):
         return FIELD_FORMATTERS[typestring(field)]
     except KeyError:
         raise ValueError('Unknown field type {0!r}'.format(field))
+
+
+def format_field_nodes(field_name, field, section_id, state):
+    """Create a section node that documents a Field config field.
+
+    Parameters
+    ----------
+    field_name : `str`
+        Name of the configuration field (the attribute name of on the config
+        class).
+    field : ``lsst.pex.config.Field``
+        A configuration field.
+    section_id : `str`
+        Unique identifier for this field. This is used as the id and name of
+        the section node.
+    state : ``docutils.statemachine.State``
+        Usually the directive's ``state`` attribute.
+
+    Returns
+    -------
+    ``docutils.nodes.section``
+        Section containing documentation nodes for the Field.
+    """
+    from lsst.pex.config import Field
+    if not isinstance(field, Field):
+        message = ('Field {0} ({1!r}) is not an '
+                   'lsst.pex.config.Field type. It is an {2!s}.')
+        raise ValueError(message.format(field_name, field, type(field)))
+
+    # Title is the field's attribute name
+    title = nodes.title(text=field_name)
+
+    dl = nodes.definition_list()
+    dl += create_default_item_node(field, state)
+    dl += create_dtype_item_node(field, state)
+    dl += create_field_type_item_node(field, state)
+
+    # Doc for this ConfigurableField, parsed as rst
+    desc_node = create_description_node(field, state)
+
+    # Package all the nodes into a `section`
+    section = make_section(
+        section_id=section_id,
+        contents=[title, dl, desc_node])
+
+    return section
 
 
 def format_configurablefield_nodes(field_name, field, section_id, state):
@@ -95,6 +142,37 @@ def format_configurablefield_nodes(field_name, field, section_id, state):
     return section
 
 
+def create_dtype_item_node(field, state):
+    """Create a definition list item node that describes a field's dtype.
+
+    Parameters
+    ----------
+    field : ``lsst.pex.config.Field``
+        A configuration field.
+    state : ``docutils.statemachine.State``
+        Usually the directive's ``state`` attribute.
+
+    Returns
+    -------
+    ``docutils.nodes.definition_list_item``
+        Definition list item that describes a field's data type.
+    """
+    type_item = nodes.definition_list_item()
+    type_item.append(nodes.term(text="Data type"))
+    if field.dtype.__module__ == 'builtins':
+        field_dtype = field.dtype.__name__
+    else:
+        field_dtype = '.'.join((field.dtype.__module__,
+                                field.dtype.__name__))
+    type_item_content = nodes.definition()
+    type_item_content += make_python_xref_nodes(
+        field_dtype,
+        state,
+        hide_namespace=False)
+    type_item.append(type_item_content)
+    return type_item
+
+
 def create_field_type_item_node(field, state):
     """Create a definition list item node that describes a field's type.
 
@@ -122,13 +200,40 @@ def create_field_type_item_node(field, state):
     return type_item
 
 
+def create_default_item_node(field, state):
+    """Create a definition list item node that describes the default value
+    of a Field config.
+
+    Parameters
+    ----------
+    field : ``lsst.pex.config.Field``
+        A configuration field.
+    state : ``docutils.statemachine.State``
+        Usually the directive's ``state`` attribute.
+
+    Returns
+    -------
+    ``docutils.nodes.definition_list_item``
+        Definition list item that describes the default target of a
+        ConfigurableField config.
+    """
+    default_item = nodes.definition_list_item()
+    default_item.append(nodes.term(text="Default"))
+    default_item_content = nodes.definition()
+    default_item_content.append(
+        nodes.literal(text=repr(field.default))
+    )
+    default_item.append(default_item_content)
+    return default_item
+
+
 def create_default_target_item_node(field, state):
     """Create a definition list item node that describes the default target
     of a ConfigurableField config.
 
     Parameters
     ----------
-    field : ``lsst.pex.config.Field``
+    field : ``lsst.pex.config.ConfigurableField``
         A configuration field.
     state : ``docutils.statemachine.State``
         Usually the directive's ``state`` attribute.
@@ -179,4 +284,6 @@ def create_description_node(field, state):
 FIELD_FORMATTERS = {
     'lsst.pex.config.configurableField.ConfigurableField':
         format_configurablefield_nodes,
+    'lsst.pex.config.config.Field':
+        format_field_nodes,
 }
