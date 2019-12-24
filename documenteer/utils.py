e94ce4da97179d22d5c4e618ccf4880f2c74c740
Jonathan Sick <jsick@lsst.org>
2019-12-24 13:39:01 -0700

    Add working_directory context manager
    This context manager provides a convenient way to change working
    directories, especially for running external programs.
    
    This is based on discussion in
    http://code.activestate.com/recipes/576620-changedirectory-context-manager/

---
+++ documenteer/utils.py
@@ -0,0 +1,40 @@
+"""Utilities used internally be Documenteer.
+"""
+
+__all__ = ('working_directory')
+
+import contextlib
+import os
+from pathlib import Path
+from typing import Union, Generator
+
+
+@contextlib.contextmanager
+def working_directory(path: Union[Path, str]) -> Generator:
+    """A context manager that temporarily changes the current working
+    directory.
+
+    Parameters
+    ----------
+    path
+        Path of the new current working directory. Either a `pathlib` path or
+        a string representation of a directory.
+
+    Examples
+    --------
+    >>> from pathlib import Path
+    >>> import os
+    >>> p = Path('mydir')
+    >>> os.makedirs(p)
+    >>> filepath = p / 'myfile.txt'
+    >>> filepath.write_text('hello')
+    >>> with working_directory(p):
+    ...     print(Path('myfile.txt').read_text())
+    hello
+    """
+    original_cwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(original_cwd)
