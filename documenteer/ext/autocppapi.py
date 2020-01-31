e03427a1c39e358133dcd1ecc752ef006e931edd
Jonathan Sick <jsick@lsst.org>
2020-01-31 13:45:04 -0700

    Cache the doxylink SymbolMap
    During the config-inited event, we can cache the SymbolMap so that each
    autocppapi directive can reference it without re-parsing the tag XML
    file.

--- documenteer/ext/autocppapi.py
+++ documenteer/ext/autocppapi.py
@@ -4,7 +4,9 @@ a namespace.
 
 __all__ = ['setup', 'AutoCppApi']
 
+from pathlib import Path
 from typing import TYPE_CHECKING, Dict, List, Any
+import xml.etree.ElementTree as ET
 
 from docutils.nodes import Node
 from docutils.parsers.rst import directives
@@ -23,6 +25,42 @@ except ImportError:
 
 if TYPE_CHECKING:
     import sphinx.application
+    import sphinx.config
+
+
+def cache_doxylink_symbolmap(
+        app: 'sphinx.application.Sphinx',
+        config: 'sphinx.config.Config',
+        ) -> None:
+    """Cache the doxylink SymbolMap used by the AutoCppApi directive, into
+    the environment.
+
+    This is connected to the ``config-inited`` event.
+    """
+    doxylink_role: str = config['documenteer_autocppapi_doxylink_role']
+    symbol_map = load_symbolmap(doxylink_role, config)
+    key = 'documenteer_autocppapi_symbolmaps'
+    if key in config:
+        if isinstance(config[key], dict):
+            config[key][doxylink_role] = symbol_map  # type: ignore
+    else:
+        config[key] = {doxylink_role: symbol_map}
+
+
+def load_symbolmap(
+    doxylink_role: str,
+    config: 'sphinx.config.Config'
+) -> doxylink.SymbolMap:
+    """Load the doxylink SymbolMap given Sphinx configuration.
+    """
+    if doxylink_role in config['doxylink']:
+        if isinstance(config['doxylink'], dict):
+            if isinstance(config['doxylink'][doxylink_role], tuple):
+                tag_path = Path(config['doxylink'][doxylink_role][0])
+                doc = ET.parse(str(tag_path))
+                return doxylink.SymbolMap(doc)
+    raise RuntimeError(
+        f'Could not load tag file for Doxylink {doxylink_role} role')
 
 
 class AutoCppApi(SphinxDirective):
@@ -56,6 +94,9 @@ def setup(app: "sphinx.application.Sphinx") -> Dict[str, Any]:
     # Configuration values
     app.add_config_value(
         'documenteer_autocppapi_doxylink_role', '', 'html')
+
+    # Events
+    app.connect('config-inited', cache_doxylink_symbolmap)
 
     # Directives
     app.add_directive('autocppapi', AutoCppApi)
