4bf65379c15c1af47ec1b07a19f72fd66bbe1d76
Jonathan Sick <jsick@lsst.org>
2022-08-26 17:49:44 -0400

    Cleanup conf for pydata theme

--- docs/conf.py
+++ docs/conf.py
@@ -100,7 +100,7 @@ html_theme = "pydata_sphinx_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {"logotext": project}
+html_theme_options = {}
 
 # in pydata-sphinx-theme 0.10.0 it'll be possible to use
 # :html_theme.sidebar_secondary.remove: metadata to remove the sidebar
@@ -109,10 +109,10 @@ html_sidebars = {"index": [], "changelog": []}
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "Documenteer"
+html_title = project
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = "Documenteer"
+html_short_title = project
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
