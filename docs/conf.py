640699d910b56ce2405ea3191698851f7c014036
Jonathan Sick <jsick@lsst.org>
2022-09-07 17:50:27 -0400

    Use documenteer.toml for intersphinx settings

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,5 @@
 from documenteer.conf.guide import *
 
-# Intersphinx
-
-intersphinx_mapping = {
-    "python": ("https://docs.python.org/3/", None),
-    "requests": ("https://requests.readthedocs.io/en/latest/", None),
-    "developer": ("https://developer.lsst.io/", None),
-    "pybtex": ("https://docs.pybtex.org/", None),
-    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
-}
-
 # Warnings to ignore
 nitpick_ignore = [
     # This link to the base pybtex still never resolves because it is not
