081252262828619e8412057ab3cef190aebbdf3a
Tim Jenness <tjenness@lsst.org>
2025-08-01 16:33:28 +0100

    Truncate long author lists in sphinx references

--- src/documenteer/ext/bibtex.py
+++ src/documenteer/ext/bibtex.py
@@ -1,14 +1,19 @@
 """Support for Rubin BibTeX files for pybtex/sphinxcontrib.bibtex."""
 
+__all__ = ["setup"]
+
 import pybtex.style.formatting.plain
-from pybtex.database import Entry
+from pybtex.database import Entry, Person
 from pybtex.plugin import register_plugin
 from pybtex.style.formatting import toplevel
 from pybtex.style.template import (
+    FieldIsMissing,
+    Node,
     field,
     first_of,
     href,
     join,
+    node,
     optional,
     optional_field,
     sentence,
@@ -19,7 +24,27 @@ from sphinx.util.typing import ExtensionMetadata
 
 from ..version import __version__
 
-__all__ = ["setup"]
+
+@node
+def truncated_names(
+    children: list[Node], context: typing.Any, role: str, **kwargs: typing.Any
+) -> Node:
+    """Return formatted names, truncating long author lists."""
+    assert not children  # noqa: S101 (matches pybtex original)
+
+    try:
+        persons = context["entry"].persons[role]
+    except KeyError:
+        raise FieldIsMissing(role, context["entry"]) from None
+
+    # If there are more than 7 authors we return the first 3.
+    if len(persons) > 7:
+        persons = persons[:3] + [Person("others")]
+    style = context["style"]
+    formatted_names = [
+        style.format_name(person, style.abbreviate_names) for person in persons
+    ]
+    return join(**kwargs)[formatted_names].format_data(context)
 
 
 class LsstBibtexStyle(pybtex.style.formatting.plain.Style):
@@ -44,6 +69,17 @@ class LsstBibtexStyle(pybtex.style.formatting.plain.Style):
             ],
         ]
         return template.format_data(e)
+
+    def format_names(self, role: str, as_sentence: bool = True) -> Node:  # noqa: FBT001, FBT002
+        # Copied from pybtex but using a different names function that
+        # caps the number of authors.
+        formatted_names = truncated_names(
+            role, sep=", ", sep2=" and ", last_sep=", and "
+        )
+        if as_sentence:
+            return sentence[formatted_names]
+        else:
+            return formatted_names
 
 
 def setup(app: Sphinx) -> ExtensionMetadata:
